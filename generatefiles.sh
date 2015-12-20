8ad050d12a06b53446a8f1c29b2c768db8d7706b
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2015-12-20 06:52:37 +0100

    added reference file for step 4

--- generatefiles.sh
+++ generatefiles.sh
@@ -1,2 +1,3 @@
-checkres2.rb cat2.res2 cat2_res2_3 3 white 1 0 0.1
-checkres2.rb cat2.res2 cat2_res2_4 4 white 0 0 0.1
+checkres2.rb cat2.res2 cat2_res2_3 3 green 0 0 0.0
+checkres2.rb cat2.res2 cat2_res2_4 4 white 0 0 0.0
+checkres2.rb cat2.res2 cat2_res2_45 4.5 white 0 0 0.1
